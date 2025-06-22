735a052a43034b4b4d680ac2fbc9e57417666955
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-21 20:58:17 -0700

    Added All

---
+++ seeds/all.ps1
@@ -0,0 +1,45 @@
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    #if not it will run the command on admin
+    Write-Warning "Run this script as Administrator!"
+    exit
+}
+
+$path = "C:/farm/wheats/wnvsh"
+
+$docs = @(
+    @{url = "https://github.com/HimadriChakra12/wnvsh/releases/download/1.0.0/wnvsh.exe" ; outfile = "$env:TEMP/wnvsh.exe"; file = "C:/farm/wheats/wnvsh/wnvsh.exe"}
+    @{url = "https://github.com/HimadriChakra12/swissknife/releases/download/0.0.5/sk.exe" ; outfile = "$env:TEMP/sk.exe"; file = "C:/farm/wheats/swissknife/sk.exe"}
+    @{url = "https://github.com/HimadriChakra12/.Pencil/releases/download/1.0.0/pencil.exe"; outfile = "$env:TEMP/pencil.exe"; file = "C:/farm/wheats/pencil/pencil.exe"}
+    @{url = "https://github.com/HimadriChakra12/.Pencil/releases/download/1.0.0/pen.exe"; outfile = "$env:TEMP/pen.exe"; file = "C:/farm/wheats/pencil/pen.exe"}
+    @{url = "https://github.com/HimadriChakra12/jux/releases/download/0.0.1/jux.exe" ; outfile = "$env:TEMP/jux.exe"; file = "C:/farm/wheats/jux/jux.exe"}
+)
+
+if (-not (test-path $path)){
+    mkdir $path | out-null
+}
+
+
+foreach ($doc in $docs){
+    iwr -uri $doc.url -OutFile $doc.outfile 
+    copy-item $doc.outfile $doc.file -force
+}
+
+try{
+    $currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
+    if ($currentPath -notlike "*$path*"){
+        [Environment]::SetEnvironmentVariable("Path", "$currentPath;$path", "User")
+        Write-Host "wnvsh added to user PATH." -ForegroundColor cyan
+    } else {
+        Write-Host "wnvsh already in user PATH." -ForegroundColor green
+    }
+} catch {
+    Write-Error "Error adding wnvsh to path: $($_.Exception.Message)"
+}
+
+if (get-command gsudo){
+    write-host "Already have gsudo" -ForegroundColor green
+} else {
+    PowerShell -Command "Set-ExecutionPolicy RemoteSigned -scope Process; [Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iwr -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | iex"
+}
+
