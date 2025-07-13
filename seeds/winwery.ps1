b1f49cad87be14835a21b17e92ec7a5ead8255c8
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-07-13 09:27:54 -0700

    Winwery

---
+++ seeds/winwery.ps1
@@ -0,0 +1,43 @@
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    #if not it will run the command on admin
+    Write-Warning "Running this script as Administrator!"
+    Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "iwr -useb "https://tinyurl.com/hwinwery" | iex "' -Verb RunAs
+    exit
+}
+
+$path = "C:/farm/wheats/winwery"
+
+$docs = @(
+    @{url = "https://github.com/HimadriChakra12/winwery/releases/download/1.0.0/wry.exe" ; outfile = "$env:TEMP/wry.exe"; file = "C:/farm/wheats/winwery/wry.exe"}
+)
+
+if (-not (test-path $path)){
+    mkdir $path | out-null
+}
+
+
+foreach ($doc in $docs){
+    iwr -uri $doc.url -OutFile $doc.outfile 
+    copy-item $doc.outfile $doc.file -force
+}
+
+try{
+    $currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
+    if ($currentPath -notlike "*$path*"){
+        [Environment]::SetEnvironmentVariable("Path", "$currentPath;$path", "User")
+        Write-Host "winwery added to user PATH." -ForegroundColor cyan
+    } else {
+        Write-Host "winwery already in user PATH." -ForegroundColor green
+    }
+} catch {
+    Write-Error "Error adding winwery to path: $($_.Exception.Message)"
+}
+
+if (get-command gsudo){
+    write-host "Already have gsudo" -ForegroundColor green
+} else {
+    PowerShell -Command "Set-ExecutionPolicy RemoteSigned -scope Process; [Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iwr -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | iex"
+}
+
+
