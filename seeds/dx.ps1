436927bd62026ba716c3e5f33d7cb283b327d91a
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-10-21 05:05:23 -0700

    Added Dx

---
+++ seeds/dx.ps1
@@ -0,0 +1,42 @@
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    #if not it will run the command on admin
+    Write-Warning "Running this script as Administrator!"
+    Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "iwr -useb "https://tinyurl.com/hdx" | iex "' -Verb RunAs
+    exit
+}
+
+$path = "C:/farm/wheats/dx"
+
+$docs = @(
+    @{url = "https://github.com/HimadriChakra12/differenciation.dx/releases/download/0.0.1/dx.exe" ; outfile = "$env:TEMP/dx.exe"; file = "C:/farm/wheats/dx/dx.exe"}
+)
+
+if (-not (test-path $path)){
+    mkdir $path | out-null
+}
+
+
+foreach ($doc in $docs){
+    iwr -uri $doc.url -OutFile $doc.outfile 
+    copy-item $doc.outfile $doc.file -force
+}
+
+try{
+    $currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
+    if ($currentPath -notlike "*$path*"){
+        [Environment]::SetEnvironmentVariable("Path", "$currentPath;$path", "User")
+        Write-Host "dx added to user PATH." -ForegroundColor cyan
+    } else {
+        Write-Host "dx already in user PATH." -ForegroundColor green
+    }
+} catch {
+    Write-Error "Error adding dx to path: $($_.Exception.Message)"
+}
+
+if (get-command gsudo){
+    write-host "Already have gsudo" -ForegroundColor green
+} else {
+    PowerShell -Command "Set-ExecutionPolicy RemoteSigned -scope Process; [Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iwr -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | iex"
+}
+
